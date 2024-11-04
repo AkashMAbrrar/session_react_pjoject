0dff7cce3e4efb111d784987939004beba8d37ac
AkashMAbrrar <madborakash48@gmail.com>
2024-11-04 10:49:08 +0600

    this is the first commmit for the all files are added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl text-center mt-8">Hello from React APP.jsx</h2>
+      <button className="btn btn-primary">Button Go</button>
+    </>
+  );
+}
+
+export default App;
