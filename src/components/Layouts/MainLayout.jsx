508813163bb7c26dd46e3e8a6e24babcc2191c55
AkashMAbrrar <madborakash48@gmail.com>
2024-11-04 12:54:47 +0600

    this commit for using react router dom and dynamic components layouts start with responsive navbar

---
+++ src/components/Layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer";
+import NavBar from "../NavBar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      {/* navBar  */}
+
+      <NavBar></NavBar>
+      <div className="min-h-[calc(100vh-232px)] py-12">
+        {/* Dynamic section */}
+        <Outlet></Outlet>
+      </div>
+
+      {/* Footer */}
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/components/Pages/Coffees.jsx
@@ -0,0 +1,9 @@
+const Coffees = () => {
+  return (
+    <div>
+      <h2 className="text-4xl">Coffees</h2>
+    </div>
+  );
+};
+
+export default Coffees;
